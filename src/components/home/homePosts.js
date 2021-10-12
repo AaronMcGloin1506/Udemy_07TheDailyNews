9ebf5f09a8010822c5ea2547a9b4f6c8ae709971
AaronMcGloin1506 <amcgloin94@gmail.com>
2021-10-12 14:49:33 +0100

    printing the posts

--- src/components/home/homePosts.js
+++ src/components/home/homePosts.js
@@ -1,9 +1,16 @@
 import React, {useEffect, useState } from 'react'; 
 import { useSelector, useDispatch} from 'react-redux';
+import Masonry from 'react-masonry-css'
+import Moment from 'react-moment';
+import { Spinner, Button } from 'react-bootstrap';
+import { LinkContainer} from 'react-router-bootstrap';
 
 import { getPosts } from '../../store/actions'
 
-import { Spinner, Button } from 'react-bootstrap';
+
+
+
+
 
 const HomePosts = () => {
 
@@ -25,6 +32,39 @@ const HomePosts = () => {
 
     return(
         <>
+            
+            <Masonry
+                breakpointCols={{default:3, 800:2, 400:1}}
+                className="my-masonry-grid"
+                columnClassName="my-masonry-grid_column">
+                { homePosts.posts ? 
+                    homePosts.posts.map((item)=>(
+                    <div key={item.id}>
+                        <img 
+                            style={{width:'100%', height:'200px '}}
+                            src={item.image}
+                        />
+                        <div className="author">
+                            <span>{item.author} -</span>
+                            <Moment format="DD MMMM">
+                                {item.createdAt}
+                            </Moment>
+                        </div>
+                        <div className="content">
+                            <div className="title">
+                                {item.title}
+                            </div>
+                            <div className="excerpt">{item.excerpt}</div>
+                            <LinkContainer
+                                to={`article/${item.id}`}
+                                className="mt-2">
+                                <Button variant='light'>Read More</Button>
+                            </LinkContainer>
+                        </div>
+                    </div>
+                )):null}
+            </Masonry>
+
             {isLoading ? 
                 <div style={{ textAlign:'center'}}>
                     <Spinner animation="border" role="status">
