38f0cf6399eea7f956706e4ac80295ca3aa88519
AaronMcGloin1506 <amcgloin94@gmail.com>
2021-10-17 12:50:50 +0100

    creating the add newsletter

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,10 +1,21 @@
 import * as api from '../../api/index';
 
 import {
-    GET_POSTS
+    GET_POSTS,
+    ADD_NEWSLETTER
     } from '../types'
+
+
+    /////Posts/////
 
 export const getPosts = (homePosts,page,order,limit) => ({
     type: GET_POSTS,
     payload: api.getPosts(homePosts,page,order,limit)
+})
+
+    /////Users/////
+
+export const addNewsLetter = (data) => ({
+    type: ADD_NEWSLETTER,
+    payload: api.addNewsLetter(data)
 })
